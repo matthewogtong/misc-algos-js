3e1b03b254a135822f7a2e2db1c4ccbfa3ce3e11
matthewogtong <ogtongm@gmail.com>
2021-08-14 16:24:43 -0400

    solve prefixStrings function

---
+++ prefixStrings.js
@@ -0,0 +1,67 @@
+/*
+For the given string s, and array of strings a, s is said to be a prefix-string of a if it is a concatenation of some prefix of the array a, i.e. if there exists some index i, such that s = a[0] + a[1] + ... + a[i].
+For example, for a = ["one", "two", "three"] strings s = "one" and s = "onetwo" are prefix-strings, while s = "two", s = "onetw", and s = "onethree" are not.
+
+Given two arrays of strings a and b, your task is to determine whether all the given strings in b are prefix-strings of a.
+
+Example
+
+For a = ["one", "two", "three"] and b = ["onetwo", "one"], the output should be prefixStrings(a, b) = true.
+
+Both of the strings b[0] = "onetwo" and b[1] = "one" are prefix-strings of a.
+
+For a = ["One", "TwoThree", "Four"] and b = ["One", "OneTwo"], the output should be prefixStrings(a, b) = false.
+
+The second string b[1] = "OneTwo" is not a prefix-string of a, since it doesn't fully match the concatenated elements of a ("OneTwoThree" would be fine, but "OneTwo" is incomplete).
+
+For a = ["One", "Two", "Three"] and b = ["Two"], the output should be prefixStrings(a, b) = false.
+
+Here is the list of prefix-strings:
+
+"One",
+"OneTwo",
+"OneTwoThree".
+"Two" isn't an option, so return false.
+Input/Output
+
+[execution time limit] 4 seconds (js)
+
+[input] array.string a
+
+An array of strings. It is guaranteed that each its element only consist of English letters.
+
+Guaranteed constraints:
+1 ≤ a.length ≤ 100,
+1 ≤ a[i].length ≤ 100.
+
+[input] array.string b
+
+An array of strings. It is guaranteed that each its element only consist of English letters.
+
+Guaranteed constraints:
+1 ≤ b.length ≤ 100,
+1 ≤ b[i].length ≤ 100.
+
+[output] boolean
+
+Return true if every element from b is prefix-string and false otherwise.
+
+*/
+
+function prefixStrings(a, b) {
+    fullComp = []
+    currentString = ''
+    
+    for (let i = 0; i < a.length; i++) {
+        currentString = currentString + a[i]
+        fullComp.push(currentString)
+    }
+    
+    for (let j = 0; j < b.length; j++) {
+        if (!(fullComp.includes(b[j]))) {
+            return false
+        }
+    }
+    
+    return true
+}
