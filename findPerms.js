aa0b4cce861c7542ec1dcccb57a564e52fa525e0
matthewogtong <ogtongm@gmail.com>
2021-08-16 23:51:07 -0400

    solve findPerms recursively

--- findPerms.js
+++ findPerms.js
@@ -1,1 +1,18 @@
 // solving findPerms recursively
+
+function findPerms(str) {
+  if (str.length === 0) return "";
+  if (str.length === 1) return str;
+  
+  let result = [];
+  for (let i = 0; i < str.length; i++) {
+    const currentChar = str[i];
+    const remainingChars = str.slice(0, i) + str.slice(i + 1);
+    for (let j = 0; j < remainingChars.length; j++) {
+      result.push(currentChar + findPerms(remainingChars)[j]);
+    }
+  }
+  return result;
+}
+
+console.log(findPerms('hello'))
